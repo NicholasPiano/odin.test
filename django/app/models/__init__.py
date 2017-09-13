0065a824294c932f57d348532fd7189ea6a73aa4
NicholasPiano <nicholas.d.piano@gmail.com>
2017-09-13 09:17:00 +0100

    Init

---
+++ django/app/models/__init__.py
@@ -0,0 +1,22 @@
+
+### Django
+from django.db import models
+
+'''
+
+The model class includes my personal serialization method and the ability to generate its own js source code.
+The model can also respond to requests for data access.
+
+'''
+
+class Model(models.Model):
+	pass
+
+	# I would like to add
+
+'''
+
+Modified database fields to include serialization and permissions.
+
+'''
+
